f8a9790d413be6181fa9ed52435cbbfe0e78da29
Amit Yadav <amit.insan@gmail.com>
2018-04-17 15:32:41 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -75,3 +75,85 @@ if(typeof lsList[id] != 'undefined') {
 objNewsletter.checkShowHook(objNewsletter.showHookArray[current]);
 
 /******************** End (Code 1) **************************/
+
+/******************** Start (Code 2) **************************/
+
+if($.inArray(2, ssoUserData.newsletter_subscribed) != -1){
+   // console.log('You have allready subscribe for daily Newsletter');
+    if($.inArray(7, ssoUserData.newsletter_subscribed) != -1){
+     //   console.log('You have allreay subscribe for market Newsletter');
+        if($.inArray(66, ssoUserData.newsletter_subscribed) != -1){
+        //    console.log('You have allreay subscribe for SlideShow Newsletter');
+            if($.inArray(1169, ssoUserData.newsletter_subscribed) != -1){
+        //        console.log('You have allreay subscribe for Market Morning');
+                if($.inArray(110, ssoUserData.newsletter_subscribed) != -1){
+                //    console.log('You have allreay subscribe for Panache');
+                    if($.inArray(28, ssoUserData.newsletter_subscribed) != -1){
+                    //    console.log('You have allreay subscribe for wealth Edition');
+                        if($.inArray(97, ssoUserData.newsletter_subscribed) != -1){
+                        //    console.log('You have allreay subscribe for Mutual Fund');
+                            if($.inArray(96, ssoUserData.newsletter_subscribed) != -1){
+                            //    console.log('You have allreay subscribe for Sunday Wrap');
+                                if($.inArray(1, ssoUserData.newsletter_subscribed) != -1){
+                                //    console.log('You have allreay subscribe for Executive Wrap');
+                                    if($.inArray(108, ssoUserData.newsletter_subscribed) != -1){
+                                    //    console.log('You have allreay subscribe for inverstment opportunities');
+                                        $('#newsWidgetSection > .newsBandSec').hide();
+                                    }else{
+                                        $('#newsWidgetSection > .newsBandSec').hide();
+                                        $('#newsWidgetSection > .newsBandSec.newsletterWidget-108').show();
+                                    }
+                                }else{
+                                    $('#newsWidgetSection > .newsBandSec').hide();
+                                    $('#newsWidgetSection > .newsBandSec.newsletterWidget-1').show();
+                                }
+                            }else{
+                                $('#newsWidgetSection > .newsBandSec').hide();
+                                $('#newsWidgetSection > .newsBandSec.newsletterWidget-96').show();
+                            }
+                        }else{
+                            $('#newsWidgetSection > .newsBandSec').hide();
+                            $('#newsWidgetSection > .newsBandSec.newsletterWidget-97').show();
+                        }
+                    }else{
+                        $('#newsWidgetSection > .newsBandSec').hide();
+                        $('#newsWidgetSection > .newsBandSec.newsletterWidget-28').show();
+                    }
+                }else{
+                    $('#newsWidgetSection > .newsBandSec').hide();
+                    $('#newsWidgetSection > .newsBandSec.newsletterWidget-110').show();
+                }
+            }else{
+                $('#newsWidgetSection > .newsBandSec').hide();
+                $('#newsWidgetSection > .newsBandSec.newsletterWidget-1169').show();
+            }
+        }else{
+            $('#newsWidgetSection > .newsBandSec').hide();
+            $('#newsWidgetSection > .newsBandSec.newsletterWidget-66').show();
+        }
+    }else{
+        $('#newsWidgetSection > .newsBandSec').hide();
+        $('#newsWidgetSection > .newsBandSec.newsletterWidget-7').show();
+    }
+}else{
+    $('#newsWidgetSection > .newsBandSec').hide();
+    $('#newsWidgetSection > .newsBandSec.newsletterWidget-2').show();
+}
+
+
+
+
+var idArray = [2, 7, 66, 1169, 110, 28, 97, 96, 1, 108];
+$.each(idArray, function (i, j) {
+    if ($.inArray(j, ssoUserData.newsletter_subscribed) == -1) {
+        $('#newsWidgetSection > .newsBandSec').hide();
+        $('#newsWidgetSection > .newsBandSec.newsletterWidget-' + j).show();
+        return false;
+    }
+})
+
+
+/******************** End (Code 2) **************************/
+
+
+
